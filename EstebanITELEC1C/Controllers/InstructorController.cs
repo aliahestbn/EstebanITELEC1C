73c6b33397b5b24fbd40f0685ec7f6eeeadabe5d
aliahestbn <aliah.esteban.cics@ust.edu.ph>
2023-09-11 12:21:35 +0800

    lab act

---
+++ EstebanITELEC1C/Controllers/InstructorController.cs
@@ -0,0 +1,48 @@
+﻿using EstebanITELEC1C.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstebanITELEC1C.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>() {
+            new Instructor()
+                {
+                Id= 1,
+                FirstName="Aliah",
+                LastName="Esteban",
+                IsTenured=true,
+                Rank=Rank.Professor,
+                HiringDate=DateTime.Parse("05/05/2023")
+            },
+            new Instructor()
+                {
+                    Id= 2,
+                FirstName="Park",
+                LastName="Jihoon",
+                IsTenured=true,
+                Rank=Rank.AssistantProfessor,
+                HiringDate=DateTime.Parse("05/05/2023")
+            }
+
+            };
+
+
+        public IActionResult Instructor()
+        {
+
+            return View(InstructorList);
+
+        }
+        public IActionResult ShowDetail(int id)
+        {
+            //Search for the student whose id matches the given id
+            Instructor? instructor = InstructorList.FirstOrDefault(st => st.Id == id);
+
+            if (instructor != null)//was an student found?
+                return View(instructor);
+
+            return NotFound();
+        }
+    }
+}
--- EstebanITELEC1C/Controllers/StudentController.cs
+++ EstebanITELEC1C/Controllers/StudentController.cs
@@ -1,12 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using EstebanITELEC1C.Models;
+
 
 namespace EstebanITELEC1C.Controllers
 {
     public class StudentController : Controller
     {
+        List<Student> StudentList = new List<Student>
+            {
+                new Student()
+                {
+                    Id= 1,FirstName = "Kang",LastName = "Daniel", Course = Course.BSIT, AdmissionDate = DateTime.Parse("2022-08-26"), GPA = 1.5, Email = "kdaniel1@gmail.com"
+                },
+                new Student()
+                {
+                    Id= 2,FirstName = "Park",LastName = "Jihoon", Course = Course.BSIS, AdmissionDate = DateTime.Parse("2022-08-07"), GPA = 1, Email = "pjihoon@gmail.com"
+                },
+                new Student()
+                {
+                    Id= 3,FirstName = "Lee",LastName = "Daehwi", Course = Course.BSCS, AdmissionDate = DateTime.Parse("2020-01-25"), GPA = 1.5, Email = "leedaehwi12@gmail.com"
+                }
+            };
+        public IActionResult Index()
+        {
+            
+            return View(StudentList);
+        }
+
         public IActionResult Student()
         {
-            return View();
+
+            return View(StudentList);
         }
+
+        public IActionResult ShowDetail(int id)
+        {
+            //Search for the student whose id matches the given id
+            Student? student = StudentList.FirstOrDefault(st => st.Id == id);
+            
+            if (student != null)//was an student found?
+                return View(student);
+
+            return NotFound();
+
+        }
+
     }
 }
+
